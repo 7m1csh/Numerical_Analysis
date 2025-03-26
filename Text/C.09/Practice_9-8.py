e9f70596c0f02dbff52473a45747dab32777ccff
shigeaki-0610076930 <0610076930@campus.ouj.ac.jp>
2025-03-26 13:23:48 +0900

    [ADD] Chapter.9 Practice.
    Added Practice9.7 and Practice9.8

---
+++ Text/C.09/Practice_9-8.py
@@ -0,0 +1,35 @@
+# Practice 9.8 Householder変換による三重対角化
+
+import numpy as np
+from scipy.linalg import hessenberg
+
+# 任意の対称行列
+A = np.array([[4, 1, 2, 3],
+              [1, 4, 3, 2],
+              [2, 3, 4, 1],
+              [3, 2, 1, 4]])
+
+# Householder変換で三重対角行列を求める
+T, Q = hessenberg(A, calc_q=True)
+
+# 数値誤差をゼロに補正
+T[np.abs(T) < 1e-10] = 0
+
+# 固有値を計算
+eig_A = np.linalg.eigvals(A)
+eig_A[np.abs(eig_A) < 1e-10] = 0  # 数値誤差をゼロに補正
+
+eig_T = np.linalg.eigvals(T)
+eig_T[np.abs(eig_T) < 1e-10] = 0  # 数値誤差をゼロに補正
+
+# 三重対角行列を作るために使用したユニタリ行列
+QtQ = Q.T @ Q
+QtQ[np.abs(QtQ) < 1e-10] = 0
+
+print(f"対称行列 A :\n{A}\n")
+print(f"三重対角行列 T :\n{T}\n")
+print("元の行列 A の固有値    :", np.sort(eig_A))
+print("三重対角行列 T の固有値:", np.sort(eig_T))
+print("")
+print(f"A を三重対角行列するために使ったユニタリ行列 Q (A = Q @ T @ Q^t):\n{Q}\n")
+print(f"Q の直交性確認　Q^t @ Q = I :\n{QtQ}")
