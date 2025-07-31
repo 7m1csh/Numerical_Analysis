52926a6ecf146a3958046873ce38f474c89b74a4
7m1csh <0610076930@campus.ouj.ac.jp>
2025-07-31 16:38:17 +0900

    [ADD] Algorithm 13.2
    Added Algorithm 13.2 and fixed the incorrect convergence judgment and boundary condition setting in Algorithm 13.1.

--- Text/C.13/Algorithm_13-2.py
+++ Text/C.13/Algorithm_13-2.py
@@ -1,6 +1,7 @@
-# Algorithm 13.1 2次元ラプラス方程式の反復解法（ヤコビ法）
+# Algorithm 13.3 2次元ラプラス方程式の反復解法（逐次加算緩和法）
 """
 input:  境界条件 $u_{00},...,u_{m+1,m+1}$ , 初期状態 $u_{11},...,u_{mm}$
+        加速緩和係数 omega
 output: ラプラス方程式を満たす u
 """
 
@@ -13,6 +14,7 @@ from matplotlib import rcParams
 rcParams['font.family'] = 'Noto Sans CJK JP'
 
 # パラメータ
+omega = 1.5
 m = 50
 max_iter = 10000
 tolerance = 1e-4
@@ -28,12 +30,12 @@ u[:, -1] = 1.0  # y = 1 の温度を1に固定（上辺）
 u[0,  :] = 0.0  # x = 0 の温度を0に固定（左辺）
 u[-1, :] = 0.0  # x = 1 の温度を0に固定（右辺）
 
-# ヤコビ法による反復
+# SOR法による反復
 for k in range(max_iter):
     for i in range(1, m):
         for j in range(1, m):
             r[i, j] = (1 / h**2 ) * (u[i+1, j] + u[i-1, j] + u[i, j+1] + u[i, j-1] - 4 * u[i, j])
-    u = u + (h**2 / 4) * r
+            u[i, j] = u[i, j] + omega * (h**2 / 4) * r[i, j]
 
     # 収束判定（最大変化量）
     diff = np.max(np.abs(r[1:-1, 1:-1]))
@@ -46,7 +48,7 @@ for k in range(max_iter):
 # 結果の表示
 plt.imshow(u.T, origin='lower', cmap='hot', extent=[0,1,0,1])
 plt.colorbar(label='Temperature')
-plt.title('２次元ラプラス方程式の反復解法（ヤコビ法）')
+plt.title(f'２次元ラプラス方程式の反復解法（SOR法）ω={omega}')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
